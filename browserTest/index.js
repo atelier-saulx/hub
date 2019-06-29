3846dddf0786e4b8f9fae50d4f7fcdca05b16330
Jim de Beer <beerdejim@gmail.com>
2019-06-29 22:13:39 +0200

    start of diff tests

--- browserTest/index.js
+++ browserTest/index.js
@@ -6,13 +6,43 @@ const hub = createClient({
   url: 'ws://localhost:6062'
 })
 
-hub.rpc('data.diff', () => {
-  console.log('received new data!')
-  console.log(hub.get('data.diff'))
-})
+hub.debug = true
+
+// hub.rpc('data.diff', () => {
+//   console.log('received new data!')
+//   console.log(hub.get('data.diff'))
+// })
+
+const List = () => {
+  const d = useRpc('data.diff', void 0, {})
+
+  if (!d.a) {
+    return 'loading...'
+  }
+
+  return (
+    <>
+      <pre>{JSON.stringify(d, false, 2)}</pre>
+      {d.a[1].items.map((v, i) => (
+        <div
+          key={i}
+          style={{
+            marginTop: 20
+          }}
+        >
+          {v.name}
+        </div>
+      ))}
+    </>
+  )
+}
 
 const App = () => {
-  return <div>test it now</div>
+  return (
+    <Provider hub={hub}>
+      <List />
+    </Provider>
+  )
 }
 
 const d = document.createElement('div')
