cfc6ae22f248e5e1ff60e34958fbb610bf43b4b6
youzi <youri@saulx.com>
2021-01-26 11:25:27 +0100

    localstorage guard

--- client/localStorage.js
+++ client/localStorage.js
@@ -1,27 +1,32 @@
 const { format } = require('./format')
 
 module.exports = (hub, params) => {
-  if (typeof window !== 'undefined' && global.localStorage) {
-    const props = format(hub, params)
-    let localValue
-
+  if (typeof window !== 'undefined') {
     try {
-      localValue = global.localStorage.getItem(props.hash)
-      if (localValue) {
-        hub.set(props, JSON.parse(localValue))
-      }
-    } catch (e) {
-      console.log('error getting from localstorage', e)
-    }
-    // want an on setting of every version of this field
-    hub.on(props, t => {
-      try {
-        global.localStorage.setItem(props.hash, JSON.stringify(t))
-      } catch (e) {
-        console.log('error setting  localstorage', e)
-      }
-    })
+      if (global.localStorage) {
+        const props = format(hub, params)
+        let localValue
 
-    return localValue
+        try {
+          localValue = global.localStorage.getItem(props.hash)
+          if (localValue) {
+            hub.set(props, JSON.parse(localValue))
+          }
+        } catch (e) {
+          console.log('error getting from localstorage', e)
+        }
+
+        // want an on setting of every version of this field
+        hub.on(props, t => {
+          try {
+            global.localStorage.setItem(props.hash, JSON.stringify(t))
+          } catch (e) {
+            console.log('error setting  localstorage', e)
+          }
+        })
+
+        return localValue
+      }
+    } catch (e) {}
   }
 }
